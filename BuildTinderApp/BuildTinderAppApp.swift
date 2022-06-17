8f8823ccb22bd2893be775ea434fda0f0f9a02d7
Ochanyoro <wadaoo0223@icloud.com>
2022-06-17 23:22:01 +0900

    Initial Commit

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by 大和田一裕 on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildTinderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildTinderApp
+//
+//  Created by 大和田一裕 on 2022/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

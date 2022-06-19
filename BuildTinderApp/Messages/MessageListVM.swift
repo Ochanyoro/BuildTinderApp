353b47b29b06f90d21003f34a4b5ecbe49eff41d
Ochanyoro <wadaoo0223@icloud.com>
2022-06-19 09:38:43 +0900

    [create] MessageListView and MessageListVM [add] init() and loadPreview into MessagePreview

---
+++ BuildTinderApp/Messages/MessageListVM.swift
@@ -0,0 +1,21 @@
+//
+//  MessageListVM.swift
+//  BuildTinderApp
+//
+//  Created by 大和田一裕 on 2022/06/19.
+//
+
+import Foundation
+
+
+class MessageListVM: ObservableObject {
+    @Published var messagePreviews: [MessagePreview] = []
+    
+    init() {
+        loadPreview()
+    }
+    
+    func loadPreview() {
+        self.messagePreviews = [MessagePreview.example]
+    }
+}
---
+++ BuildTinderApp/Messages/MessageListView.swift
@@ -0,0 +1,24 @@
+//
+//  MessageListView.swift
+//  BuildTinderApp
+//
+//  Created by 大和田一裕 on 2022/06/19.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    var body: some View {
+        VStack {
+            Text("search bar")
+            
+            Text("Vstack of all our conversations")
+        }
+    }
+}
+
+struct MessageListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageListView()
+    }
+}

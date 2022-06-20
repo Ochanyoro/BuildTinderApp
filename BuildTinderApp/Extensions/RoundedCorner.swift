b9ca6698f0d58a054fd8c1b303ffd11a74b69327
Ochanyoro <wadaoo0223@icloud.com>
2022-06-20 22:49:49 +0900

    [edit]

---
+++ BuildTinderApp/Extensions/RoundedCorner.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedCorner.swift
+//  BuildTinderApp
+//
+//  Created by 大和田一裕 on 2022/06/20.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
